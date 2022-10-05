3ee70591b5f609a6b234c32d251801302acc3c51
SeanyBeany <24shawn@hotmail.ca>
2022-10-05 20:35:15 -0300

    added Basic printing to testingStrings

--- testingStrings.c
+++ testingStrings.c
@@ -7,5 +7,12 @@
 #define BUFSIZE 100
 
 int main(int argc, char *argv[]) {
-
+    int c=37;
+    printf("Enter a character: ");
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
+    return 0;
 }
