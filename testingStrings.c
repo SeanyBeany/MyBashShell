d1c5c2d7a623bb42edf5a1861c512098e299d7fa
SeanyBeany <24shawn@hotmail.ca>
2022-10-05 23:42:16 -0300

    Getting closer to making strtok_r work

--- testingStrings.c
+++ testingStrings.c
@@ -6,13 +6,53 @@
 
 #define BUFSIZE 100
 
-int main(int argc, char *argv[]) {
-    int c=37;
-    printf("Enter a character: ");
+int main(int argc, char *argv[]){
     
-    // %d displays the integer value of a character
-    // %c displays the actual character
-    printf("ASCII value of %c = %d", c, c);
+    int ret_code;		// return code
+    int len;		// length of entered command
+    char buffer[BUFSIZE];	// room for 80 chars plus \0
+    char *cmd;		// pointer to entered command
+    int pid;
+    char cwd[BUFSIZE];
+    static int percentage = 37; //Ascii value for percentage
+    /**
+    getcwd(cwd, sizeof(cwd));
+    printf("%s%c", cwd,percentage);
+    cmd = fgets(buffer, BUFSIZE, stdin);
     
-    return 0;
+    while(cmd != NULL){       
+        int ret_code;		// return code
+        int len;		// length of entered command
+        char buffer[BUFSIZE];	// room for 80 chars plus \0
+        char *cmd;		// pointer to entered command
+        int pid;
+        char cwd[BUFSIZE];
+        static int percentage = 37; //Ascii value for percentage
+        char* token;
+        char* str;
+
+        if(cmd != NULL){
+            // check for the newline character and overwrite with \0
+            len = strlen(buffer);
+            if(buffer[len-1] == '\n'){
+                buffer[len-1] = '\0';
+            }
+        }
+        getcwd(cwd, sizeof(cwd));
+        printf("%s%c", cwd, percentage);
+
+        cmd = fgets(buffer, BUFSIZE, stdin);
+        //tokenizes the command string
+        str = cmd;
+        */
+
+    cmd = fgets(buffer, BUFSIZE, stdin);
+    char* token;
+    char* rest = cmd;
+    while ((token = strtok_r(rest, " ", &rest)))
+        printf("%s\n", token);
+    return (0);
+
 }
+
+
