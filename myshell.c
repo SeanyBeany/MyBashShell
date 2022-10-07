0621327c6e0be37b1a5a48858153af224e1f19b3
SeanyBeany <24shawn@hotmail.ca>
2022-10-07 13:13:01 -0300

    Fully  working part 3

--- myshell.c
+++ myshell.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define BUFSIZE 100
+#define BUFSIZE 1000
 #define EXIT_SUCCESS 0
 #define COMMAND_LIMIT 20
 
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]){
     int len;		// length of entered command
     char buffer[BUFSIZE];	// room for 80 chars plus \0
     char *cmd;		// pointer to entered command
-    pid_t pid;
+    pid_t pid;  //pid of the current child or parent process
     char cwd[BUFSIZE]; //Contains path to current working directory
     static int percentage = 37; //Ascii value for percentage
     char* command[COMMAND_LIMIT]; //Array of all the strings to execute a system command with execvp
@@ -48,60 +48,58 @@ int main(int argc, char *argv[]){
         //tokenizes the command string
         char *p = cmd;
         char* tokens[BUFSIZE];
-
+        int tokenCounter = 0; //Counter for tokens
         for(int i = 0; i < BUFSIZE; i++){
             tokens[i] = strtok_r(p, " ", &p);
             if(tokens[i] == NULL){
                 break;
             }
+            tokenCounter++;
         }
-        
+
         //cd command implementation
         if(strcmp(tokens[0], "cd") == 0){
             chdir(tokens[1]);
         }
         
         //exit commands implementation
-        if(strcmp("exit", tokens[0]) == 0){
+        else if(strcmp("exit", tokens[0]) == 0){
             printf("Program has successfully exited\n");
             return EXIT_SUCCESS;
         }
     
-        
-        pid = fork(); 
-        if(pid == -1){
-            perror("error creating child process");
-            return 2;
-        }
-
-        if(pid == 0){
-            command[0] = tokens[0];
-            command[1] = tokens[1];
-            command[2] = NULL;
-            ret_code = execvp(tokens[0], command);
-            if(ret_code == -1){
-                perror("error executing command");
-                fgets(buffer, BUFSIZE, stdin);
-                exit(1);
-            }
-            else{
-                exit(0);
-            }
-        }
         else{
-            wait(&status);
+            pid = fork(); //create child process
+            if(pid == -1){ //fork failed, therefore print error message
+                perror("error creating child process");
+                return 2;
+            }
+            // Child process
+            if(pid == 0){
+                //Puts every string together in an array to execute command
+                for(int i =0; i< tokenCounter; i++){
+                command[i] = tokens[i];
+                }
+                command[tokenCounter] = NULL;//The array needs to terminate with NULL
+                ret_code = execvp(tokens[0], command); // execute command
+                if(ret_code == -1){ //execute failed, so print error message
+                    perror("error executing command");
+                    exit(1);
+                }
+                else{ //execute success return 0
+                    exit(0);
+                }
+            }
+            // Parent process
+            else{  
+                wait(&status); //Parent is waiting on child process
+            }
         }
 
         
     }
-
-    
-    /**
-    int what = strcmp(tokens[0], "ls");
-    printf("%d",what);
-    ret_code = execlp(tokens[0], tokens[0], NULL);
-    */
-    return EXIT_SUCCESS;
-
+    //Incase of error which causes loop break, print error message
+    printf("Error occured, ending program");
+    return -1; 
 }
 
