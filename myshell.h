06ead5e47ee439271b03fc672e2ab5ec318d8b2a
SeanyBeany <24shawn@hotmail.ca>
2022-10-07 18:01:27 -0300

    created myshell.h header file

---
+++ myshell.h
@@ -0,0 +1,10 @@
+#ifndef MYSHELL_H
+#define MYSHELL_H
+
+//main function for the shell
+int main(int argc, char *argv[])();
+
+//Function that prints current directory using getcwd function
+void printDirectory();
+
+#endif
